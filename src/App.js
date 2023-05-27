f894c62b5b54b30a5ae0451c7302430a61eb00b9
Jasdeepkaur1381 <kjasdeep219@gmail.com>
2023-05-27 08:52:40 +0530

    To do List with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+  const inputEvent = (e) => {
+    setInputList(e.target.value);
+  };
+  const ListOfItems = () => {
+    setItems((preVal) => {
+      return [...preVal, inputList];
+    });
+    setInputList("");
+
+  };
+  const deleteItems = (e) => {
+    console.log(e.target);
+    console.log("Deleted");
+    setItems((preVal) => {
+      return preVal.filter((item, index) => {
+        return item !== e.target.value;
+      });
+    });
+    console.log(items);
+  }
+  return <>
+    <div className='outerDiv'>
+      <div className='innerDiv'>
+        <h1>ToDo List</h1>
+
+        <input type='text' placeholder='Enter a Event' onChange={inputEvent} value={inputList} />
+        <button onClick={ListOfItems}> + </button>
+        <ol>{
+          items.map((itemval, index) => {
+            return (<>
+              <div className='item-style' key={index}>
+                <i className="fa-solid fa-circle-xmark" value={itemval} onClick={deleteItems}></i>
+                <li>{itemval}</li>
+              </div>
+
+            </>);
+          })}
+          {/* <li>{inputList}</li> */}
+        </ol>
+      </div>
     </div>
-  );
+  </>;
 }
 
 export default App;
